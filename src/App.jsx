e05290032158d5a36ae4db08a34117b568834301
Luke <lucasporfirioa@gmail.com>
2026-05-06 15:40:46 -0300

    fix: updating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import StudentCard from './pages/StudentCard'
 import Validation from './pages/Validation'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<StudentCard />} />
         <Route path="/validation" element={<Validation />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
